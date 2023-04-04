8e118aaeaea2e1e2f656f639646ebbd3a0c84605
Branden Knight <brandenkn@gmail.com>
2023-04-03 18:50:35 -0600

    Updated education section styling

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,23 +3,23 @@ import React from "react";
 const Education = () => {
   return (
     <section id="Education" className="container py-5 education-container">
-      <h2>Education</h2>
-      <div>
+      <h2 className="section-title">Education</h2>
+      <div className="education-item">
         <h3>B.S.B.A. Project Management</h3>
         <h4>Park University</h4>
         <p>2021 - Current</p>
       </div>
-      <div>
+      <div className="education-item">
         <h3>Electrical Systems Technology</h3>
         <h4>Community College of the Air Force</h4>
         <p>2019 - 2023</p>
       </div>
-      <div>
+      <div className="education-item">
         <h3>Computer Science</h3>
         <h4>Westchester Community College</h4>
         <p>2016 - 2018</p>
       </div>
-      <div>
+      <div className="education-item">
         <h3>High School</h3>
         <h4>Pelham Memorial High School</h4>
         <p>2015</p>
